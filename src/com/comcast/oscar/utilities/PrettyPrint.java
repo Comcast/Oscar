0ff35df527f469a87a9aae604428ab8c8afb5fd5
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-04-19 11:33:46 -0400

    Fixed Null insertion found

--- src/com/comcast/oscar/utilities/PrettyPrint.java
+++ src/com/comcast/oscar/utilities/PrettyPrint.java
@@ -2,16 +2,15 @@ package com.comcast.oscar.utilities;
 
 import java.util.StringTokenizer;
 
-public class PrettyPrint {
+public class PrettyPrint extends StringTokenizer {
 
-	private static final String START_INDENT_TOKEN	= "{";
-	private static final String END_INDENT_TOKEN 	= "}";
-	private static final String NEW_LINE_TOKEN 		= ";";
-	private static final String OPEN_COMMENT_SLASH 	= "/*";
-	private static final String CLOSE_COMMENT_SLASH = "*/";
+	private final String START_INDENT_TOKEN		= "{";
+	private final String END_INDENT_TOKEN 		= "}";
+	private final String NEW_LINE_TOKEN 		= ";";
+	private final String OPEN_COMMENT_SLASH 	= "/*";
+	private final String CLOSE_COMMENT_SLASH 	= "*/";
 	
-	private String sInputCode ;
-	private String sOutputCode ;
+	private String sOutputCode = "";
 	private int iIndentCurrentDepth = 0;
 
 	
@@ -19,10 +18,11 @@ public class PrettyPrint {
 	 * 
 	 * @param sbCode
 	 */
-	public PrettyPrint(String sInputCode) {
+	public PrettyPrint(String sInputCode)  {		
+		super(sInputCode	.replaceAll("\\s+", " ")
+							.replaceAll("\\*/", " */")
+							.replaceAll(";", " ;"));
 		
-		this.sInputCode = sInputCode;
-				
 		start();
 	}
 	
@@ -39,64 +39,37 @@ public class PrettyPrint {
 	private void start() {
 		
 		boolean boolCloseCommentSlash = false;
-		
-		//Remove all long whitespace and clean up each token
-		this.sInputCode = this.sInputCode	.replaceAll("\\s+", " ")
-											.replaceAll("\\*/", " */")
-											.replaceAll(";", " ;");
-		
-		//Create token Stream..bringing legacy back!!!!
-		StringTokenizer stConfiguration = new StringTokenizer(sInputCode);
-		
+				
 		//Cycle thru each token
-		while (stConfiguration.hasMoreTokens()) {
+		while (super.hasMoreTokens()) {
 			
-			String sToken = stConfiguration.nextToken();
-
+			String sToken = super.nextToken();
+			
 			//Check for open curly brace
-			if (START_INDENT_TOKEN.equals(sToken)) {
-				
-				sOutputCode += indentation(iIndentCurrentDepth);
-				
-				sOutputCode += (sToken);
-				
-				sOutputCode += ('\n');
-				
-				iIndentCurrentDepth++;
-				
-				sOutputCode += (indentation(iIndentCurrentDepth));
-			
+			if (START_INDENT_TOKEN.equals(sToken)) {				
+				sOutputCode += indentation(iIndentCurrentDepth);				
+				sOutputCode += (sToken);				
+				sOutputCode += ('\n');				
+				iIndentCurrentDepth++;				
+				sOutputCode += (indentation(iIndentCurrentDepth));			
 			//Check for close curly brace
 			} else if (END_INDENT_TOKEN.equals(sToken)) {
-		
 				sOutputCode += (indentation(--iIndentCurrentDepth));
-				
-				sOutputCode += (sToken);
-				
-				sOutputCode += ('\n');
-			
+				sOutputCode += (sToken);				
+				sOutputCode += ('\n');			
 			//Check for semi-colon
-			} else if (CLOSE_COMMENT_SLASH.equals(sToken)) {
-				
-				sOutputCode += (sToken);
-				
-				sOutputCode += ('\n');
-				
-				//sOutputCode += (indentation(iIndentCurrentDepth));
-				
+			} else if (CLOSE_COMMENT_SLASH.equals(sToken)) {				
+				sOutputCode += (sToken);				
+				sOutputCode += ('\n');						
 				boolCloseCommentSlash = true;
-
-			} else if (NEW_LINE_TOKEN.equals(sToken)) {
-				
+			} else if (NEW_LINE_TOKEN.equals(sToken)) {				
 				sOutputCode += (sToken);
 				sOutputCode += ('\t');
-			} else {
-				
+			} else {				
 				if (boolCloseCommentSlash) {
 					sOutputCode += (indentation(iIndentCurrentDepth));
 					boolCloseCommentSlash = false;
-				}
-				
+				}				
 				sOutputCode += (" " + sToken);
 			}	
 		}	
