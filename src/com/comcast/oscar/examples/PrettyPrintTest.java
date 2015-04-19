cd35227cab78521729f6232b7c86114d20d9c3a3
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-04-19 17:07:14 -0400

    Provided Both Bin <-> Text Code Exampels

--- src/com/comcast/oscar/examples/PrettyPrintTest.java
+++ src/com/comcast/oscar/examples/PrettyPrintTest.java
@@ -1,23 +1,53 @@
 package com.comcast.oscar.examples;
 
+import java.io.FileNotFoundException;
+
+import com.comcast.oscar.configurationfile.ConfigrationFileException;
+import com.comcast.oscar.configurationfile.ConfigrationFileImport;
+import com.comcast.oscar.configurationfile.ConfigurationFile;
 import com.comcast.oscar.configurationfile.ConfigurationFileExport;
 import com.comcast.oscar.test.TestDirectoryStructure;
 import com.comcast.oscar.utilities.HexString;
+import com.comcast.oscar.utilities.PrettyPrint;
 
 public class PrettyPrintTest {
 
 	public static void main(String[] args) {
 
-		HexString hs = new HexString(HexString.fileToByteArray(TestDirectoryStructure.fInputDirFileName("bsod.txt")));
+		/***************************
+		 * Text to Binary
+		 ***************************/
+		ConfigrationFileImport cfi = null;
+		try {
+			cfi = new ConfigrationFileImport(TestDirectoryStructure.fInputDirFileName("bsod.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ConfigrationFileException e) {
+			e.printStackTrace();
+		}
 		
-		//PrettyPrint pp = new PrettyPrint(hs.toASCII());
+		ConfigurationFile cf = new ConfigurationFile(ConfigurationFile.DOCSIS_VER_30,cfi.getTlvBuilder());
 		
-		//System.out.println(pp.toString());
+		ConfigurationFileExport cfe =  new ConfigurationFileExport (cf);
 		
-		ConfigurationFileExport cfe = new ConfigurationFileExport (TestDirectoryStructure.fInputDirFileName("bsod.cm"));
+		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_FOUND_TLV));
+		System.out.println("+-------------------------------------------------------------------------------------------+");
+		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_DEFAULT_TLV));
+
+		System.out.println("+-------------------------------------------------------------------------------------------+");		
+		System.out.println("+-------------------------------------------------------------------------------------------+");		
+		System.out.println("+-------------------------------------------------------------------------------------------+");		
+		System.out.println("+-------------------------------------------------------------------------------------------+");		
+
+		/***************************
+		 * Binary to Text
+		 ***************************/
+		cfe = new ConfigurationFileExport (TestDirectoryStructure.fInputDirFileName("bsod.cm"));
 		
 		//Does not Export Default TLVs
 		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_FOUND_TLV));
+		System.out.println("+-------------------------------------------------------------------------------------------+");		
+		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_DEFAULT_TLV));
 		
 	}
 
