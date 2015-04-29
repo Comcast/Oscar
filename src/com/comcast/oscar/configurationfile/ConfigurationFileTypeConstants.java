cc506b608c710490bfc2fbb1354af9339766a31a
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-04-29 11:30:08 -0400

    Updated ConfigurationFileExport

---
+++ src/com/comcast/oscar/configurationfile/ConfigurationFileTypeConstants.java
@@ -0,0 +1,20 @@
+package com.comcast.oscar.configurationfile;
+
+public class ConfigurationFileTypeConstants {
+
+	public static final Integer DOCSIS_10_CONFIGURATION_TYPE 	= 0;
+	public static final Integer DOCSIS_11_CONFIGURATION_TYPE 	= 1;
+	public static final Integer DOCSIS_20_CONFIGURATION_TYPE 	= 2;
+	public static final Integer DOCSIS_30_CONFIGURATION_TYPE 	= 3;
+	public static final Integer DOCSIS_31_CONFIGURATION_TYPE 	= 4;
+	
+	public static final Integer	PKT_CABLE_10_CONFIGURATION_TYPE	= 100;
+	public static final Integer	PKT_CABLE_15_CONFIGURATION_TYPE	= 101;
+	public static final Integer	PKT_CABLE_20_CONFIGURATION_TYPE	= 102;
+	
+	public static final Integer DPOE_10_CONFIGURATION_TYPE 		= 200;
+	public static final Integer DPOE_20_CONFIGURATION_TYPE 		= 201;
+	
+	public static final Integer DPOG_10_CONFIGURATION_TYPE 		= 300;
+	
+}
