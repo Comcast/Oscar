ecf4d7b6edfcd1240be1861c3fd7297ff86d7042
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-05-06 15:11:00 -0400

    Initial

--- src/com/comcast/oscar/netsnmp/Constants.java
+++ src/com/comcast/oscar/netsnmp/Constants.java
@@ -4,8 +4,10 @@ import com.comcast.oscar.utilities.DirectoryStructure;
 
 public class Constants {
 	
-	public final static String MIB_PARAMETER 			= " -M all " + DirectoryStructure.fMibsTextDir();
-	public final static String SNMP_TRANSLATE_PARAMETER = " -Ta ";
+	public final static String SNMP_TRANSLATE_CMD =  "snmptranslate";
+	public final static String MIB_PARAMETER = " -m ALL -M " + DirectoryStructure.fMibsTextDir();
+	public final static String SNMP_TRANSLATE_OID_NAME_2_OID_DEC = " -IR -On ";
+	public final static String SNMP_TRANSLATE_OID_DEC_2_OID_NAME = " -Oas ";
 	public final static String SNMP_TRANSLATE_DEBUG 	= " -D all ";
 	public final static String SNMP_TRANSLATE_VERSION 	= " -V ";
 	
--- src/com/comcast/oscar/netsnmp/NetSNMP.java
+++ src/com/comcast/oscar/netsnmp/NetSNMP.java
@@ -1,10 +1,101 @@
 package com.comcast.oscar.netsnmp;
 
-public class NetSNMP {
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class NetSNMP extends ArrayList<String> {
+
+	private static final long serialVersionUID = 1L;
+	private Boolean debug = Boolean.FALSE;
+	
+	/**
+	 * 
+	 */
 	public NetSNMP() {
-		
+		super();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<String> OidName2OidDec() {
+		String sSnmpTranslate = Constants.SNMP_TRANSLATE_CMD + 	
+								Constants.MIB_PARAMETER + 
+								Constants.SNMP_TRANSLATE_OID_NAME_2_OID_DEC +
+								super.toString().replace(',', ' ').replace('[', ' ').replace(']', ' ');
+		
+		return runSnmpTranslate(sSnmpTranslate);
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<String> OidDec2OidName() {
+		
+		String sSnmpTranslate = Constants.SNMP_TRANSLATE_CMD +  	
+								Constants.MIB_PARAMETER + 
+								Constants.SNMP_TRANSLATE_OID_NAME_2_OID_DEC +
+								super.toString().replace(',', ' ').replace('[', ' ').replace(']', ' ');
+		
+		return runSnmpTranslate(sSnmpTranslate);
+
+	}
+
+	/**
+	 * 
+	 * @param sSnmpTranslateCMD
+	 * @return
+	 */
+	private ArrayList<String> runSnmpTranslate(String sSnmpTranslateCMD) {
+		
+		ArrayList<String> als = new ArrayList<String>();
+		
+		Process p = null;
+		try {
+			p = Runtime.getRuntime().exec(sSnmpTranslateCMD);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+		String sStd_IO = "";
+
+		/*Read the output from the command If Any */
+
+		int iCounter = 0;
+		try {
+			while ((sStd_IO = stdInput.readLine()) != null) {
+				
+				als.add(sStd_IO);
+				
+				if (debug)
+					System.out.println(++iCounter + ": " + sStd_IO);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			while ((sStd_IO = stdError.readLine()) != null) {
+				
+				als.add(sStd_IO);
+				
+				if (debug)
+					System.out.println("OUT:" + sStd_IO);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return als;
+	}
 }
+
