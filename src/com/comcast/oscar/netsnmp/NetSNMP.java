659b883d7f15a026355affa1cf3fc4e09fa869ba
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-05-07 07:39:49 -0400

    Revert Back

--- src/com/comcast/oscar/netsnmp/NetSNMP.java
+++ src/com/comcast/oscar/netsnmp/NetSNMP.java
@@ -37,9 +37,6 @@ public class NetSNMP extends ArrayList<String> {
 				.replace(']', ' ')
 				.replaceAll("\\s+1", " .iso")
 				.replaceAll("\\s+\\.1", " .iso");
-		
-		if (Boolean.TRUE)
-			System.out.println(sSnmpTranslate);
 
 		return runSnmpTranslate(sSnmpTranslate);
 	}
@@ -60,10 +57,7 @@ public class NetSNMP extends ArrayList<String> {
 				Constants.SNMP_TRANSLATE_OID_NAME_2_OID_DEC +
 				sOID.replaceAll("\\s+1", " .iso")
 					.replaceAll("\\s+\\.1", " .iso");
-	
-		if (Boolean.TRUE)
-			System.out.println(sSnmpTranslate);
-		
+
 		return runSnmpTranslate(sSnmpTranslate).get(0);
 	}
 
@@ -73,7 +67,7 @@ public class NetSNMP extends ArrayList<String> {
 	 * 
 	 * @return ArrayList<String> -> docsDevNmAccessIp.1 ....
 	 */
-	public DottedToTextualOIDMap toTextualOID() {
+	public ArrayList<String> toTextualOID() {
 
 		String sSnmpTranslate = Constants.SNMP_TRANSLATE_CMD +  	
 				Constants.MIB_PARAMETER + 
@@ -89,7 +83,7 @@ public class NetSNMP extends ArrayList<String> {
 
 	/**
 	 * 
-	 * @param sOID - Input MUST be a DottedOID - .1.3.6
+	 * @param sOID
 	 * @return
 	 */
 	public static String toTextualOID(String sOID) {
@@ -99,10 +93,10 @@ public class NetSNMP extends ArrayList<String> {
 				Constants.SNMP_TRANSLATE_OID_DEC_2_OID_NAME +
 				sOID;
 
-		if (Boolean.TRUE)
-			System.out.println(sSnmpTranslate);
+		if (true)
+			System.out.println("NetSNMP.OidDec2OidName(): " + sSnmpTranslate);
 
-		return runSnmpTranslate(sSnmpTranslate).get(sOID);
+		return runSnmpTranslate(sSnmpTranslate).get(0);
 
 	}
 
@@ -139,14 +133,14 @@ public class NetSNMP extends ArrayList<String> {
 	 * @param sSnmpTranslateCMD
 	 * @return
 	 */
-	private DottedToTextualOIDMap runSnmpTranslate(String sSnmpTranslateCMD) {
+	private static ArrayList<String> runSnmpTranslate(String sSnmpTranslateCMD) {
 
 		boolean localDebug = Boolean.FALSE;
-		
-		DottedToTextualOIDMap dttom = new DottedToTextualOIDMap(super.);
 
 		if (debug|localDebug)
 			System.out.println(sSnmpTranslateCMD);
+
+		ArrayList<String> als = new ArrayList<String>();
 
 		Process p = null;
 		try {
@@ -163,16 +157,13 @@ public class NetSNMP extends ArrayList<String> {
 
 		/*Read the output from the command If Any */
 
-		int iCounter = 0,
-			iIndex = 0;
+		int iCounter = 0;
 		try {
 			while ((sStd_IO = stdInput.readLine()) != null) {
 
 				//Clean up White Space
-				if (!sStd_IO.isEmpty()) {
-					/* Add to Mapping */
-					dttom.put(super.get(iIndex++),sStd_IO);
-				}
+				if (!sStd_IO.isEmpty())
+					als.add(sStd_IO);
 
 				if (debug|localDebug)
 					System.out.println(++iCounter + " IN: " + sStd_IO);
@@ -184,6 +175,8 @@ public class NetSNMP extends ArrayList<String> {
 		try {
 			while ((sStd_IO = stdError.readLine()) != null) {
 
+				als.add(sStd_IO);
+
 				if (debug|localDebug)
 					System.out.println(++iCounter + " OUT: " + sStd_IO);
 			}
@@ -191,7 +184,7 @@ public class NetSNMP extends ArrayList<String> {
 			e.printStackTrace();
 		}
 
-		return dttom;
+		return als;
 	}
 }
 
