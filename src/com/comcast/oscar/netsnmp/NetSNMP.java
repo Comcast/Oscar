751f0873539f6efd534686eaf16639a6d79554d9
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-05-07 00:18:01 -0400

    Update

--- src/com/comcast/oscar/netsnmp/NetSNMP.java
+++ src/com/comcast/oscar/netsnmp/NetSNMP.java
@@ -37,6 +37,9 @@ public class NetSNMP extends ArrayList<String> {
 				.replace(']', ' ')
 				.replaceAll("\\s+1", " .iso")
 				.replaceAll("\\s+\\.1", " .iso");
+		
+		if (Boolean.TRUE)
+			System.out.println(sSnmpTranslate);
 
 		return runSnmpTranslate(sSnmpTranslate);
 	}
@@ -57,7 +60,10 @@ public class NetSNMP extends ArrayList<String> {
 				Constants.SNMP_TRANSLATE_OID_NAME_2_OID_DEC +
 				sOID.replaceAll("\\s+1", " .iso")
 					.replaceAll("\\s+\\.1", " .iso");
-
+	
+		if (Boolean.TRUE)
+			System.out.println(sSnmpTranslate);
+		
 		return runSnmpTranslate(sSnmpTranslate).get(0);
 	}
 
@@ -67,7 +73,7 @@ public class NetSNMP extends ArrayList<String> {
 	 * 
 	 * @return ArrayList<String> -> docsDevNmAccessIp.1 ....
 	 */
-	public ArrayList<String> toTextualOID() {
+	public DottedToTextualOIDMap toTextualOID() {
 
 		String sSnmpTranslate = Constants.SNMP_TRANSLATE_CMD +  	
 				Constants.MIB_PARAMETER + 
@@ -93,10 +99,10 @@ public class NetSNMP extends ArrayList<String> {
 				Constants.SNMP_TRANSLATE_OID_DEC_2_OID_NAME +
 				sOID;
 
-		if (true)
-			System.out.println("NetSNMP.OidDec2OidName(): " + sSnmpTranslate);
+		if (Boolean.TRUE)
+			System.out.println(sSnmpTranslate);
 
-		return runSnmpTranslate(sSnmpTranslate).get(0);
+		return runSnmpTranslate(sSnmpTranslate).get(sOID);
 
 	}
 
@@ -133,14 +139,14 @@ public class NetSNMP extends ArrayList<String> {
 	 * @param sSnmpTranslateCMD
 	 * @return
 	 */
-	private static ArrayList<String> runSnmpTranslate(String sSnmpTranslateCMD) {
+	private DottedToTextualOIDMap runSnmpTranslate(String sSnmpTranslateCMD) {
 
 		boolean localDebug = Boolean.FALSE;
+		
+		DottedToTextualOIDMap dttom = new DottedToTextualOIDMap(super.);
 
 		if (debug|localDebug)
 			System.out.println(sSnmpTranslateCMD);
-
-		ArrayList<String> als = new ArrayList<String>();
 
 		Process p = null;
 		try {
@@ -157,13 +163,16 @@ public class NetSNMP extends ArrayList<String> {
 
 		/*Read the output from the command If Any */
 
-		int iCounter = 0;
+		int iCounter = 0,
+			iIndex = 0;
 		try {
 			while ((sStd_IO = stdInput.readLine()) != null) {
 
 				//Clean up White Space
-				if (!sStd_IO.isEmpty())
-					als.add(sStd_IO);
+				if (!sStd_IO.isEmpty()) {
+					/* Add to Mapping */
+					dttom.put(super.get(iIndex++),sStd_IO);
+				}
 
 				if (debug|localDebug)
 					System.out.println(++iCounter + " IN: " + sStd_IO);
@@ -175,8 +184,6 @@ public class NetSNMP extends ArrayList<String> {
 		try {
 			while ((sStd_IO = stdError.readLine()) != null) {
 
-				als.add(sStd_IO);
-
 				if (debug|localDebug)
 					System.out.println(++iCounter + " OUT: " + sStd_IO);
 			}
@@ -184,7 +191,7 @@ public class NetSNMP extends ArrayList<String> {
 			e.printStackTrace();
 		}
 
-		return als;
+		return dttom;
 	}
 }
 
