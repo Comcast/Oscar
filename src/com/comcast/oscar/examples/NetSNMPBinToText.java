ab84427ec9aaa74e97c21ac076c9b3426170a3eb
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-05-07 11:51:02 -0400

    Added more Jackson Packages

--- src/com/comcast/oscar/examples/NetSNMPBinToText.java
+++ src/com/comcast/oscar/examples/NetSNMPBinToText.java
@@ -7,7 +7,9 @@ public class NetSNMPBinToText {
 
 	public static void main(String[] args) {
 
-		ConfigurationFileExport cfe = new ConfigurationFileExport (TestDirectoryStructure.fInputDirFileName("bsod.cm"),ConfigurationFileExport.DOCSIS_VER_31);
+		ConfigurationFileExport cfe = 
+				new ConfigurationFileExport (TestDirectoryStructure.fInputDirFileName("DOCSIS-GOLDEN.bin"),
+											 ConfigurationFileExport.DOCSIS_VER_31);
 		
 		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_FOUND_TLV));
 
--- src/com/comcast/oscar/netsnmp/NetSNMP.java
+++ src/com/comcast/oscar/netsnmp/NetSNMP.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+
 public class NetSNMP extends ArrayList<String> {
 
 	/**
