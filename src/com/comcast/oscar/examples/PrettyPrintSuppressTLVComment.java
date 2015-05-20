4f992c87a8c275732edb2d093757d4012cd03d66
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-05-20 14:16:37 -0400

    Update Banner

--- src/com/comcast/oscar/examples/PrettyPrintSuppressTLVComment.java
+++ src/com/comcast/oscar/examples/PrettyPrintSuppressTLVComment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.comcast.oscar.examples;
 
 import com.comcast.oscar.configurationfile.ConfigurationFileExport;
@@ -22,33 +21,6 @@ import com.comcast.oscar.test.TestDirectoryStructure;
  * @author Maurice Garcia (maurice.garcia.2015@gmail.com)
  */
 
-
-public class PrettyPrintSuppressTLVComment {
-
-	public static void main(String[] args) {
-		
-		/***************************
-		 * Binary to Text
-		 ***************************/
-		@SuppressWarnings("deprecation")
-		ConfigurationFileExport cfe = new ConfigurationFileExport (TestDirectoryStructure.fInputDirFileName("cm.bin"));
-			
-		/* No Suppression */
-		System.out.println(cfe.toPrettyPrint(ConfigurationFileExport.EXPORT_FOUND_TLV));
-
-		/* With Suppression */
-		System.out.println(cfe.toPrettyPrint(	ConfigurationFileExport.EXPORT_FOUND_TLV,
-												ConfigurationFileExport.SUPPRESS_TLV_COMMENT));
-
-
-	}
-
-}
-=======
-package com.comcast.oscar.examples;
-
-import com.comcast.oscar.configurationfile.ConfigurationFileExport;
-import com.comcast.oscar.test.TestDirectoryStructure;
 
 public class PrettyPrintSuppressTLVComment {
 
