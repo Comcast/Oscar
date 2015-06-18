568eafe872ece4b3599e9391687decb758fffc10
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-06-18 12:53:57 -0400

    Added clearJsonDB() to clear Text/Dotted OID Mapping

--- src/com/comcast/oscar/netsnmp/NetSNMP.java
+++ src/com/comcast/oscar/netsnmp/NetSNMP.java
@@ -2,8 +2,10 @@ package com.comcast.oscar.netsnmp;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -280,6 +282,27 @@ public class NetSNMP  {
 		return sDescription;	
 	}
 
+	/**
+	 * Clear DottedTextualNetSNMPMap.json and start new
+	 */
+	public static void clearJsonDB() {
+		
+		if (DirectoryStructureNetSNMP.fNetSNMPJSON().exists()) {
+					
+			PrintWriter pw = null;
+			try {
+				pw = new PrintWriter(DirectoryStructureNetSNMP.fNetSNMPJSON());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			pw.print("");
+			pw.close();
+			
+		}
+		
+		FixNullNetSNMPJSON();
+	}
+	
 	/**
 	 * 
 	 * @param sSnmpTranslateCMD
