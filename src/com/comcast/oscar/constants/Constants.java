62f7cbc14818d325f7dac87006175d7408e9f0f3
Maurice Garcia <mgarcia01752@outlook.com>
2026-02-05 09:33:07 -0700

    release: v2.2.13

--- src/com/comcast/oscar/constants/Constants.java
+++ src/com/comcast/oscar/constants/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 
   private static final String TAB = "\t";
 
-  public static final String OSCAR_VERSION = "2.2.12";
+  public static final String OSCAR_VERSION = "2.2.13";
   public static final String OSCAR_CLI_USAGE =
       "java -jar oscar.jar -k <key> -i <.txt> -o <.bin> -c";
   public static final String OSCAR_CLI_HEADER =
